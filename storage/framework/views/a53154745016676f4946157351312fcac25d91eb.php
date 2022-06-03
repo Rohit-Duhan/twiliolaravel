501a4da01715f17e3b17ad42dc27a66ca6d9d4c8
Rohit Duhan <69465655+Rohit-Duhan@users.noreply.github.com>
2022-06-03 11:19:16 +0530

    Add files via upload

---
+++ storage/framework/views/a53154745016676f4946157351312fcac25d91eb.php
@@ -0,0 +1,25 @@
+<?php $__env->startSection('content'); ?>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header"><?php echo e(__('Dashboard')); ?></div>
+
+                <div class="card-body">
+                    <?php if(session('status')): ?>
+                        <div class="alert alert-success" role="alert">
+                            <?php echo e(session('status')); ?>
+
+                        </div>
+                    <?php endif; ?>
+                    <h2>
+                        <a href="<?php echo e(url('/sms')); ?>" style="color: blue;cursor: pointer;">Click here for send SMS.</a>
+                    </h2>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\twiliolaravel\twilio\twilio\resources\views/home.blade.php ENDPATH**/ ?>
